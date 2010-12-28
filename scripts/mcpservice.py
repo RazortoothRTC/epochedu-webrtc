682eccf2950f21ebf183eca90a6aacf44cf79a69
David J. Kordsmeier <dkords@gmail.com>
2010-12-28 00:32:50 -0800

    Add initial example for JSON POST.  Need to handle it properly for all user

--- scripts/mcpservice.py
+++ scripts/mcpservice.py
@@ -26,18 +26,30 @@ import cherrypy
 import os
 import json
 
+class StreamFilter(cherrypy.filters.basefilter.BaseFilter):
+	def before_request_body(self):
+		if cherrypy.request.path == '/rpc':
+			if not 'Content-Length' in cherrypy.request.headerMap or \
+				(cherrypy.request.method != 'POST'):
+				raise cherrypy.HTTPRedirect('/'):
+			else:
+				cherrypy.request.processRequestBody = False
+		
+
 class MCPService(object):
     def __init__(self):
         self.droid = android.Android()
 
     """ Sample request handler class. """
     def index(self):
-        return """<html><head><title>An example application</title></head>
+        return """<html><head><title>MCP Service</title></head>
 <body>
-<h1>This is my sample application, Hello World!</h1>
-Put the content here...
+<h1>Services Overview</h1>
+Put services documentation here.
 <hr>
-<a href="/exit">Quit</a>
+<h1>Administration</h1>
+<a href="/dumpqueue">Queue Dump</a>
+<a href="/exit">Shutdown</a>
 </body></html>"""
     index.exposed = True
 
@@ -45,11 +57,42 @@ Put the content here...
         raise SystemExit(0)
     exit.exposed = True
 
-def testviewcraigslist():
-	aURL = 'http://craigslist.com'
-	aMIME = 'text/html'
-    droid.view(aURL, aMIME) 
-
+	def testviewcraigslist(selfd):
+		aURL = 'http://craigslist.com'
+		aMIME = 'text/html'
+	    droid.view(aURL, aMIME) 
+	testviewcraigslist.exposed = True
+	
+	def launchurl(self, aurl, amime):
+		if aurl is None return
+		if amime is None:
+			droid.view(aurl)
+		else:
+			droid.view(aurl, amime)
+	
+	def rpc(self):
+		dataLength = int(cherrypy.request.headers.get('Content-Length') or 0)
+		data = cherrypy.request.rfile.read(dataLength)
+		o = json.dumps(data)
+		
+		requestID = o.apdu
+		if requestID is None:
+			pass
+		if requestID == 1:
+			launchurl(o.launchurl, None)
+		if requestID == 2:
+			pass
+		if requestID == 3:
+			pass
+		if requestID == 4:
+			pass
+		if requestID == 5:
+			pass
+		if requestID == 6
+			pass
+		
+		
+		
 def run():
     cherrypy.config.update({'cherrypy.server.socket_port':'8080'})
     cherrypy.config.update({'server.socket_host':'127.0.0.1'})
