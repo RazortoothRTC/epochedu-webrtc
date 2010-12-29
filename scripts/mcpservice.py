318c316a4977346b63210875088f75b7c0a743ce
David J. Kordsmeier <dkords@gmail.com>
2010-12-28 21:02:43 -0800

    Updated MCP service to run with android module or not.  Using cherrypy and set up basic Tool to check the incoming RPC request to ensure it is valid. need to implement the JSON.

--- scripts/mcpservice.py
+++ scripts/mcpservice.py
@@ -20,56 +20,68 @@ import logging
 # we must disable multiprocessing logging
 logging.logMultiprocessing = 0
  
-import android
-import webbrowser # XXX Leave this in, it may be useful
+try:
+	import android
+except:
+	import webbrowser # XXX Leave this in, it may be useful for linux/mac testing
 import cherrypy
 import os
 import json
 
-class StreamFilter(cherrypy.filters.basefilter.BaseFilter):
-	def before_request_body(self):
-		if cherrypy.request.path == '/rpc':
-			if not 'Content-Length' in cherrypy.request.headerMap or \
+
+def print_path(multiplier=1):
+    for i in range(multiplier):
+        print cherrypy.request.path_info
+cherrypy.tools.print_path = cherrypy.Tool('on_start_resource', print_path)
+
+def validate_rpc():
+		if cherrypy.request.path_info == '/rpc':
+			if not 'Content-Length' in cherrypy.request.headers or \
 				(cherrypy.request.method != 'POST'):
-				raise cherrypy.HTTPRedirect('/'):
+				raise cherrypy.HTTPRedirect('/')
 			else:
 				cherrypy.request.processRequestBody = False
-		
+cherrypy.tools.validate_rpc = cherrypy.Tool('before_request_body', validate_rpc)
+
+def check_access(default=False):
+	print "check_access"
+	if not getattr(cherrypy.request, "userid", default):
+		raise cherrypy.HTTPError(401)
+	newauthtools.check_access = cherrypy.Tool('before_request_body', check_access)
+
+_cp_config = {'tools.sessions.on': True}
 
 class MCPService(object):
-    def __init__(self):
-        self.droid = android.Android()
+	def __init__(self):
+		# self.droid = android.Android()
+		pass
 
-    """ Sample request handler class. """
-    def index(self):
-        return """<html><head><title>MCP Service</title></head>
+	""" Basic MCP Service - need to add auth """
+	@cherrypy.expose
+	def index(self):
+		return """<html><head><title>MCP Service</title></head>
 <body>
-<h1>Services Overview</h1>
+<h1>MCP Services Overview</h1>
 Put services documentation here.
 <hr>
 <h1>Administration</h1>
 <a href="/dumpqueue">Queue Dump</a>
 <a href="/exit">Shutdown</a>
 </body></html>"""
-    index.exposed = True
 
-    def exit(self):
-        raise SystemExit(0)
-    exit.exposed = True
+	def exit(self):
+		raise SystemExit(0)
+	exit.exposed = True
 
-	def testviewcraigslist(selfd):
+	def testviewcraigslist(self):
 		aURL = 'http://craigslist.com'
 		aMIME = 'text/html'
-	    droid.view(aURL, aMIME) 
+	    # droid.view(aURL, aMIME) 
+		print "droid view launched"
 	testviewcraigslist.exposed = True
 	
-	def launchurl(self, aurl, amime):
-		if aurl is None return
-		if amime is None:
-			droid.view(aurl)
-		else:
-			droid.view(aurl, amime)
-	
+	@cherrypy.expose
+	@cherrypy.tools.validate_rpc()
 	def rpc(self):
 		dataLength = int(cherrypy.request.headers.get('Content-Length') or 0)
 		data = cherrypy.request.rfile.read(dataLength)
@@ -88,16 +100,32 @@ Put services documentation here.
 			pass
 		if requestID == 5:
 			pass
-		if requestID == 6
+		if requestID == 6:
 			pass
 		
+		# Set the response header
+		# set the data body
+		# send response
 		
+	def launchurl(self, aurl, amime):
+		if aurl is None: return
+		if amime is None:
+			try:
+				droid.view(aurl)
+			except:
+				webbrowser.open(aurl)  
+			print "droid view launched with url"
+		else:
+			try:
+				droid.view(aurl, amime)
+			except:
+				webbrowser.open(aurl)  
+			print "droid view launched with mime type + url"
 		
 def run():
     cherrypy.config.update({'cherrypy.server.socket_port':'8080'})
     cherrypy.config.update({'server.socket_host':'127.0.0.1'})
     cherrypy.quickstart(MCPService(), '/')
-    cherrypy.engine.subscribe('start', browse, priority=90)
     cherrypy.engine.block()
     #os.system('python script.py')
 
