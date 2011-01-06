8dfe1f843bae1e459938c313e654bc478264d5fb
David J. Kordsmeier <devnull@localhost>
2011-01-05 17:58:44 -0800

    Fixy

--- static/js/epoch-client.js
+++ static/js/epoch-client.js
@@ -592,10 +592,10 @@ function longPoll (data) {
 						// $('#waiting').text("");
 						$('#dialog').find('#waiting').remove();
 					}
-				// } /* else {
+				/* } else {
 					// If you aren't logged in, bump back to login screen
 					showLogin();
-				} */
+				}  */
 			} else {
 				// DO something in case there are two teachers
 			}
