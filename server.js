296facec7a57d72f8681815529d79e5d692c504c
David J. Kordsmeier <devnull@localhost>
2011-01-06 13:47:45 -0800

    Removed fixed footer in place of inline style.

--- server.js
+++ server.js
@@ -101,11 +101,12 @@ var starttime = (new Date()).getTime();
 //
 // VERSION - generic version string for support and QA
 //
-VERSION = "ces2011-marvell-v13-b23-" + starttime ;  // XXX Can  we instrument this using hudson during packaging, maybe use commit GUID
+VERSION = "ces2011-marvell-v13-b24-" + starttime ;  // XXX Can  we instrument this using hudson during packaging, maybe use commit GUID
 WIP = "MCP command work in progress.\n \
 		Doing some fixes with regards to messages + session.\n \
 		Prep for CES setup.\n \
 	 	Undo session management. \
+		Add fixed inline footer. \
 ";
 var DEFAULT_CHANNEL = 'default';
 var BOTNICK = "robot"
