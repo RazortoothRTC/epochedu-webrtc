b591d6bc7101bcbf6dce4642cecbfe47d47243f2
David J. Kordsmeier <dkords@gmail.com>
2011-02-01 13:00:53 -0800

    Fix for launchurl.  Works now, but only with a valid mime type extensions.

--- scripts/mcpservice.py
+++ scripts/mcpservice.py
@@ -83,7 +83,174 @@ MCP_CONFIG = {'ANDROID_CONTENT_PATH':'/sdcard/content',
 			  'MCP_TICK_INTERVAL' : 30, # Seconds between ticks DEMOSETUP
 			  'CONTENT_REPO_LOCAL_URL' : "content://com.android.htmlfileprovider", 
 			  'ANDROID_VIEW_ACTIVITY' : 'android.intent.action.VIEW', # These are documented in Android Dev Docs
-			  'VALID_FILE_EXTENSIONS' : ['.jpg', '.gif', '.png', '.mov', '.mp3', '.wav', '.mp4', '.flv', '.html', '.tif', '.apk', '.txt', '.doc', '.rtf', '.pdf'], 
+			  'VALID_FILE_EXTENSIONS' : ['.jpg', '.gif', '.png', '.mov', '.mp3', '.wav', '.mp4', '.flv', '.html', '.tif', '.apk', '.txt', '.doc', '.rtf', '.pdf'],
+			  'VALID_MIME_TYPES' : {    ".3gp"   : "video/3gpp" # BORROWED FROM fu.js (see source for epochedu-master)
+			          				  , ".a"     : "application/octet-stream"
+							          , ".ai"    : "application/postscript"
+							          , ".aif"   : "audio/x-aiff"
+							          , ".aiff"  : "audio/x-aiff"
+							          , ".asc"   : "application/pgp-signature"
+							          , ".asf"   : "video/x-ms-asf"
+							          , ".asm"   : "text/x-asm"
+							          , ".asx"   : "video/x-ms-asf"
+							          , ".atom"  : "application/atom+xml"
+							          , ".au"    : "audio/basic"
+							          , ".avi"   : "video/x-msvideo"
+							          , ".bat"   : "application/x-msdownload"
+							          , ".bin"   : "application/octet-stream"
+							          , ".bmp"   : "image/bmp"
+							          , ".bz2"   : "application/x-bzip2"
+							          , ".c"     : "text/x-c"
+							          , ".cab"   : "application/vnd.ms-cab-compressed"
+							          , ".cc"    : "text/x-c"
+							          , ".chm"   : "application/vnd.ms-htmlhelp"
+							          , ".class"   : "application/octet-stream"
+							          , ".com"   : "application/x-msdownload"
+							          , ".conf"  : "text/plain"
+							          , ".cpp"   : "text/x-c"
+							          , ".crt"   : "application/x-x509-ca-cert"
+							          , ".css"   : "text/css"
+							          , ".csv"   : "text/csv"
+							          , ".cxx"   : "text/x-c"
+							          , ".deb"   : "application/x-debian-package"
+							          , ".der"   : "application/x-x509-ca-cert"
+							          , ".diff"  : "text/x-diff"
+							          , ".djv"   : "image/vnd.djvu"
+							          , ".djvu"  : "image/vnd.djvu"
+							          , ".dll"   : "application/x-msdownload"
+							          , ".dmg"   : "application/octet-stream"
+							          , ".doc"   : "application/msword"
+							          , ".dot"   : "application/msword"
+							          , ".dtd"   : "application/xml-dtd"
+							          , ".dvi"   : "application/x-dvi"
+							          , ".ear"   : "application/java-archive"
+							          , ".eml"   : "message/rfc822"
+							          , ".eps"   : "application/postscript"
+							          , ".exe"   : "application/x-msdownload"
+							          , ".f"     : "text/x-fortran"
+							          , ".f77"   : "text/x-fortran"
+							          , ".f90"   : "text/x-fortran"
+							          , ".flv"   : "video/x-flv"
+									  , ".apk"	 : "application/vnd.android.package-archive"
+							          , ".for"   : "text/x-fortran"
+							          , ".gem"   : "application/octet-stream"
+							          , ".gemspec" : "text/x-script.ruby"
+							          , ".gif"   : "image/gif"
+							          , ".gz"    : "application/x-gzip"
+							          , ".h"     : "text/x-c"
+							          , ".hh"    : "text/x-c"
+							          , ".htm"   : "text/html"
+							          , ".html"  : "text/html"
+							          , ".ico"   : "image/vnd.microsoft.icon"
+							          , ".ics"   : "text/calendar"
+							          , ".ifb"   : "text/calendar"
+							          , ".iso"   : "application/octet-stream"
+							          , ".jar"   : "application/java-archive"
+							          , ".java"  : "text/x-java-source"
+							          , ".jnlp"  : "application/x-java-jnlp-file"
+							          , ".jpeg"  : "image/jpeg"
+							          , ".jpg"   : "image/jpeg"
+							          , ".js"    : "application/javascript"
+							          , ".json"  : "application/json"
+							          , ".log"   : "text/plain"
+							          , ".m3u"   : "audio/x-mpegurl"
+							          , ".m4v"   : "video/mp4"
+							          , ".man"   : "text/troff"
+							          , ".mathml"  : "application/mathml+xml"
+							          , ".mbox"  : "application/mbox"
+							          , ".mdoc"  : "text/troff"
+							          , ".me"    : "text/troff"
+							          , ".mid"   : "audio/midi"
+							          , ".midi"  : "audio/midi"
+							          , ".mime"  : "message/rfc822"
+							          , ".mml"   : "application/mathml+xml"
+							          , ".mng"   : "video/x-mng"
+							          , ".mov"   : "video/quicktime"
+							          , ".mp3"   : "audio/mpeg"
+							          , ".mp4"   : "video/mp4"
+							          , ".mp4v"  : "video/mp4"
+							          , ".mpeg"  : "video/mpeg"
+							          , ".mpg"   : "video/mpeg"
+							          , ".ms"    : "text/troff"
+							          , ".msi"   : "application/x-msdownload"
+							          , ".odp"   : "application/vnd.oasis.opendocument.presentation"
+							          , ".ods"   : "application/vnd.oasis.opendocument.spreadsheet"
+							          , ".odt"   : "application/vnd.oasis.opendocument.text"
+							          , ".ogg"   : "application/ogg"
+							          , ".p"     : "text/x-pascal"
+							          , ".pas"   : "text/x-pascal"
+							          , ".pbm"   : "image/x-portable-bitmap"
+							          , ".pdf"   : "application/pdf"
+							          , ".pem"   : "application/x-x509-ca-cert"
+							          , ".pgm"   : "image/x-portable-graymap"
+							          , ".pgp"   : "application/pgp-encrypted"
+							          , ".pkg"   : "application/octet-stream"
+							          , ".pl"    : "text/x-script.perl"
+							          , ".pm"    : "text/x-script.perl-module"
+							          , ".png"   : "image/png"
+							          , ".pnm"   : "image/x-portable-anymap"
+							          , ".ppm"   : "image/x-portable-pixmap"
+							          , ".pps"   : "application/vnd.ms-powerpoint"
+							          , ".ppt"   : "application/vnd.ms-powerpoint"
+							          , ".ps"    : "application/postscript"
+							          , ".psd"   : "image/vnd.adobe.photoshop"
+							          , ".py"    : "text/x-script.python"
+							          , ".qt"    : "video/quicktime"
+							          , ".ra"    : "audio/x-pn-realaudio"
+							          , ".rake"  : "text/x-script.ruby"
+							          , ".ram"   : "audio/x-pn-realaudio"
+							          , ".rar"   : "application/x-rar-compressed"
+							          , ".rb"    : "text/x-script.ruby"
+							          , ".rdf"   : "application/rdf+xml"
+							          , ".roff"  : "text/troff"
+							          , ".rpm"   : "application/x-redhat-package-manager"
+							          , ".rss"   : "application/rss+xml"
+							          , ".rtf"   : "application/rtf"
+							          , ".ru"    : "text/x-script.ruby"
+							          , ".s"     : "text/x-asm"
+							          , ".sgm"   : "text/sgml"
+							          , ".sgml"  : "text/sgml"
+							          , ".sh"    : "application/x-sh"
+							          , ".sig"   : "application/pgp-signature"
+							          , ".snd"   : "audio/basic"
+							          , ".so"    : "application/octet-stream"
+							          , ".svg"   : "image/svg+xml"
+							          , ".svgz"  : "image/svg+xml"
+							          , ".swf"   : "application/x-shockwave-flash"
+							          , ".t"     : "text/troff"
+							          , ".tar"   : "application/x-tar"
+							          , ".tbz"   : "application/x-bzip-compressed-tar"
+							          , ".tcl"   : "application/x-tcl"
+							          , ".tex"   : "application/x-tex"
+							          , ".texi"  : "application/x-texinfo"
+							          , ".texinfo" : "application/x-texinfo"
+							          , ".text"  : "text/plain"
+							          , ".tif"   : "image/tiff"
+							          , ".tiff"  : "image/tiff"
+							          , ".torrent" : "application/x-bittorrent"
+							          , ".tr"    : "text/troff"
+							          , ".txt"   : "text/plain"
+							          , ".vcf"   : "text/x-vcard"
+							          , ".vcs"   : "text/x-vcalendar"
+							          , ".vrml"  : "model/vrml"
+							          , ".war"   : "application/java-archive"
+							          , ".wav"   : "audio/x-wav"
+							          , ".wma"   : "audio/x-ms-wma"
+							          , ".wmv"   : "video/x-ms-wmv"
+							          , ".wmx"   : "video/x-ms-wmx"
+							          , ".wrl"   : "model/vrml"
+							          , ".wsdl"  : "application/wsdl+xml"
+							          , ".xbm"   : "image/x-xbitmap"
+							          , ".xhtml"   : "application/xhtml+xml"
+							          , ".xls"   : "application/vnd.ms-excel"
+							          , ".xml"   : "application/xml"
+							          , ".xpm"   : "image/x-xpixmap"
+							          , ".xsl"   : "application/xml"
+							          , ".xslt"  : "application/xslt+xml"
+							          , ".yaml"  : "text/yaml"
+							          , ".yml"   : "text/yaml"
+							          , ".zip"   : "application/zip"
+							          }, 
 			  'BROWSER_PLAYER_EXTENSIONS' : ['.jpg', '.gif', '.png', '.tif', '.apk', '.txt', '.html'], # These are things that only play in the browser
 			'ASCII_LOGO' : """
 			@#@#++@@@@@@@@@@@
@@ -123,14 +290,14 @@ class MCPService(object):
 	# XXX Does cherrypy have some kind of config file thingy?
 	ANDROID_CONTENT_PATH = '/sdcard/content'
 	DESKTOP_CONTENT_PATH = '/tmp'
-	VERSION_TAG = 'ces2011-r7-b7-' + datetime.datetime.now().isoformat()
+	VERSION_TAG = 'ces2011-r7-b8-' + datetime.datetime.now().isoformat()
 	VERSION_DESC = """
 	ISANDROID = False
 	<P>Fixed breakage from CES, and change handling of rpc to properly return a JSON response.  JSONFIY tool for 
 	CherryPy doesn't really work well.  I'd like to get rid of CherryPy.  Implement pingheartbeat command.
 	Implement basic functionality in launchurl to call into getbesturlpath to check the local cache for content.
 	Fix some bad stuff in getbesturlpath.  Added ISANDROID.  Fix broken notification.  Sync works now.
-	Working on killplatformplayer.  Bug fixes on launchviewer.
+	Working on killplatformplayer.  Bug fixes on launchviewer ...
 	</P>
 	"""
 	# XXX Cleanup this duplicate config code, move it into global MCP_CONFIG
@@ -260,7 +427,7 @@ Todo ...
 			jsonResp['status'] = -1
 		print 'apdu received: ' + apdu
 		if apdu == '1':
-			jsonResp = self.launchurl(params['launchurl'], None, jsonResp)
+			jsonResp = self.launchurl(params['launchurl'], jsonResp)
 		if apdu == '2':
 			print 'sync value is ' + params['sync']
 			jsonResp = self.syncContent(params['sync'], jsonResp)
@@ -313,9 +480,9 @@ Todo ...
 	#
 	# MCP apdu Handlers
 	#
-	def launchurl(self, aurl, amime, rsp):
+	def launchurl(self, aurl, rsp):
 		if aurl is None: rsp.status = -1
-		
+		amime = None
 		# Get the best url path
 		aurl = self.getbesturlpath(aurl)
 		print 'launchurl is opening best url path ' + aurl
@@ -323,21 +490,20 @@ Todo ...
 		if aurl is None:
 			rsp['status'] = 1 # Launch in the browser
 			return rsp
-		if amime is None:
+		try:
+			amime = MCP_CONFIG['VALID_MIME_TYPES'][aurl[aurl.rindex('.'):0]]
+		except KeyError, e:
+			pass
+		if amime is not None:
 			try:
-				print 'launching without mime type'
-				self.droid.view(aurl)
-			except:
-				webbrowser.open(aurl)
-			print "droid view launched with url" + aurl
-		else:
-			try:
-				print 'launching with mime type ' + amime
 				self.droid.view(aurl, amime)
 			except:
-				webbrowser.open(aurl)  
-			print "droid view launched with mime type + url"
-		rsp['status'] = 0
+				webbrowser.open(aurl)
+			print "droid view launched with url" + aurl + ' and mime = ' + amime
+			rsp['status'] = 0
+		else:
+			print "can't launch a url without a mime type ... to unpredictable in SL4A"
+			rsp['status'] = -1
 		return rsp
 		
 	def syncContent(self, urls, rsp):
@@ -479,7 +645,7 @@ Todo ...
 		if channel is not None:
 			if self.ISANDROID:
 				channelpath = MCP_CONFIG['ANDROID_CONTENT_PATH'] + '/' + channel
-				contentrepourl = MCP_CONFIG['CONTENT_REPO_LOCAL_URL']
+				contentrepourl = "" # XXX For launching activity, just use a local path to sdcard
 			else:
 				channelpath = MCP_CONFIG['DESKTOP_CONTENT_PATH'] + '/' + channel
 				contentrepourl = 'file://'
