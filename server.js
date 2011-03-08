44931ba947153a67eb257c7deca63804afdcaa38
David J. Kordsmeier <devnull@localhost>
2011-03-08 11:10:34 -0800

    fix for stuttering restart of video player.  First run a blank video.

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ var starttime = (new Date()).getTime();
 //
 // VERSION - generic version string for support and QA
 //
-VERSION = "epochedu-marvell-ces-stable-demo-v3-b81-" + starttime ;  // XXX Can  we instrument this using hudson during packaging, maybe use commit GUID
+VERSION = "epochedu-marvell-ces-stable-demo-v3-b82-" + starttime ;  // XXX Can  we instrument this using hudson during packaging, maybe use commit GUID
 WIP = " <li>MCP command work completed.</li>\n \
 		<li>Incorporating feedback for crayola demo from customer</li> \n \
 		<li>Remove Cufon </li>\n \
@@ -182,6 +182,7 @@ WIP = " <li>MCP command work completed.</li>\n \
 		<li>Fix for long poll error notification handling </li> \n \
 		<li>Added growl status notification for low memory situations </li> \n \
 		<li>Fix for sync folder PDF Launch bug</li> \n \
+		<li>Fix for stutter stop on video.</li> \n \
 ";
 var DEFAULT_CHANNEL = 'default';
 var BOTNICK = "robot"
