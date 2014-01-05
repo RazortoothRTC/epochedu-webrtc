7f8f05bcfd51ecb4bb920744e50e2d60783c381b
David J. Kordsmeier <dkords@gmail.com>
2014-01-04 17:57:39 -0800

    refs #1 Add screencapture script for linux.

---
+++ screencapsvcs/asl-1.2/run-screencap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Android Screenshot Library #
+##############################
+#       Startup script       #
+
+echo "Android Screenshot Library -- initializing..."
+#
+if [ -z $ANDROID ]; then
+	echo "*** Android SDK not found ***"
+	echo "Make sure the ANDROID variable is pointing to Android SDK root directory"
+else
+	adb="$ANDROID/platform-tools/adb"
+	
+	# Check whether device is connected or wait for one
+	adbState=`$adb get-state`
+	if [ $adbState = "device" ]; then
+		echo "Device not found -- connect one to continue..."
+		$adb wait-for-device
+		echo "Device connected."
+	fi
+	
+	# Install service
+	echo "Installing native service..."
+	$adb push ./asl-native /data/local/tmp/screencaploop.sh
+	$adb shell /system/bin/chmod 0777 /data/local/tmp/screencaploop.sh
+	
+	# Start the service
+	echo "Starting service..."
+	# $adb shell "/data/local/tmp/screencaploop.sh /data/local/tmp/screencaploop.sh.log" &
+	adb shell "while [ 1 ]; do /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-1.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-2.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-3.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-4.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-5.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-6.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-7.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-8.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-9.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeedsscreen-10.png; sleep 1; done" &
+
+	echo "Service started successfully."
+fi
