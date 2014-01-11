d865392270aa15d96177dc35048dd57d1ee849af
David J. Kordsmeier <dkords@gmail.com>
2014-01-11 13:58:11 -0800

    refs #29 Use the correct file:// path, not file://localhost.

--- mcp/scripts/mcpservice.py
+++ mcp/scripts/mcpservice.py
@@ -529,7 +529,7 @@ Todo ...
 		try:
 			self.droid = android.Android()
 			channelpath = MCP_CONFIG['ANDROID_CONTENT_PATH'] + '/' + channel
-			contentrepourl = MCP_CONFIG['CONTENT_REPO_LOCAL_URL3']
+			contentrepourl = MCP_CONFIG['CONTENT_REPO_LOCAL_URL2']
 		except:
 			channelpath = MCP_CONFIG['DESKTOP_CONTENT_PATH'] + '/' + channel
 			contentrepourl = 'file://'
