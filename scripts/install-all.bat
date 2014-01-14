0a93bff8a3c906d26763e4fa0bc5f132521461c3
David J. Kordsmeier <dkords@gmail.com>
2014-01-14 00:13:10 -0800

    refs #70 Add install-all.bat
    Tested on win 7 and 8.  Need an associated .sh script

---
+++ scripts/install-all.bat
@@ -0,0 +1,44 @@
+@echo off
+
+echo Installing EpochEDU support APKs for Android
+
+if not defined ANDROID goto SdkPathNotDefined
+
+echo Waiting for device to be connected...
+%ANDROID%\platform-tools\adb.exe wait-for-device
+
+echo - Installing Mozilla apk ...
+%ANDROID%\platform-tools\adb.exe install ../android/fennec-28.0a2.en-US.android-arm.apk
+
+echo - Installing OI File Manager apk ...
+%ANDROID%\platform-tools\adb.exe install ../android/FileManager-2.0.2.apk
+
+echo - Installing EpochWatchdog apk ...
+%ANDROID%\platform-tools\adb.exe install ../android/EpochWatchdog-debug.apk
+
+echo - Installing sl4a apk ...
+%ANDROID%\platform-tools\adb.exe install ../android/sl4a_r6.apk
+
+echo - Installing SMILE Teacher apk ...
+%ANDROID%\platform-tools\adb.exe install ../android/SMILETeacher-0.9.9b1.apk
+
+REM Unfortunately, we need to install the python part manually
+REM echo - Installing sl4a python apk ...
+REM %ANDROID%\platform-tools\adb.exe install ../android/PythonForAndroid_r4.apk
+
+echo - Installing cherrypy for sl4a
+%ANDROID%\platform-tools\adb.exe push ./cherrypy /mnt/sdcard/sl4a/scripts/cherrypy
+
+echo - Installing mcpservice.py for sl4a
+%ANDROID%\platform-tools\adb.exe push ./mcpservice.py /mnt/sdcard/sl4a/scripts/mcpservice.py
+
+echo - Installing demoreset.py for sl4a
+%ANDROID%\platform-tools\adb.exe push ./demoreset.py /mnt/sdcard/sl4a/scripts/demoreset.py
+
+goto Finish
+                                                                                                                                                                                                                                                                                                                     
+:SdkPathNotDefined
+echo *** Android SDK not found ***
+echo Make sure ANDROID variable is set to root folder of Android SDK.
+
+:Finish
