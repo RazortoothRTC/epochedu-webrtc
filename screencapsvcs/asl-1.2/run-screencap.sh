38aaec32c0f93278a4c9057220cc5003df6533e1
David J. Kordsmeier <dkords@gmail.com>
2014-01-17 15:29:27 -0800

    refs #73 Fix the associated .sh script to mkdir the right dir.

--- screencapsvcs/asl-1.2/run-screencap.sh
+++ screencapsvcs/asl-1.2/run-screencap.sh
@@ -21,13 +21,13 @@ else
 	fi
 	
 	# Install service
-	echo "Installing native service..."
-	$adb push ./asl-native /data/local/tmp/screencaploop.sh
-	$adb shell /system/bin/chmod 0777 /data/local/tmp/screencaploop.sh
+	echo "starting..."
+	# $adb push ./asl-native /data/local/tmp/screencaploop.sh
+	# $adb shell /system/bin/chmod 0777 /data/local/tmp/screencaploop.sh
 	
 	# Start the service
-	echo "Starting service..."
 	# $adb shell "/data/local/tmp/screencaploop.sh /data/local/tmp/screencaploop.sh.log" &
+	adb shell "mkdir -p /mnt/sdcard/sl4a/scripts/mcpfeeds"
 	adb shell "while [ 1 ]; do /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-1.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-2.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-3.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-4.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-5.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-6.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-7.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-8.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-9.png; sleep 1; /system/bin/screencap -p /mnt/sdcard/sl4a/scripts/mcpfeeds/screen-10.png; sleep 1; done &" &
 
 	echo "Service started successfully."
