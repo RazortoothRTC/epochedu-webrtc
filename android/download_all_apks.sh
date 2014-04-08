9d2059e90e74306f5c114cc638f7efeb23a46b12
David J. Kordsmeier <dkords@gmail.com>
2014-04-07 23:59:06 -0700

    refs #refs #101 Clean out large apks,  Reduce repo footprint

---
+++ android/download_all_apks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget http://polyblog.s3.amazonaws.com/fennec-30.0a2.en-US.android-arm.apk
+wget http://polyblog.s3.amazonaws.com/EpochWatchdog-debug.apk
+wget http://polyblog.s3.amazonaws.com/MCPServiceActivity-debug.apk
+wget http://polyblog.s3.amazonaws.com/FileManager-2.0.2.apk
+wget http://polyblog.s3.amazonaws.com/SMILETeacher-gti-debug.apk
